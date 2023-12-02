944c641bf1f1eafdd84210502bc0528478cfd3f7
desktop <ryo.kokai.g@gmail.com>
2023-12-02 23:02:45 +0900

    単体テスト時にDBのレコード数もチェックするように修正。

--- blog/tests.py
+++ blog/tests.py
@@ -1,13 +1,9 @@
 import os
 from django.core.management import call_command
 from rest_framework.test import APITestCase
-from django.urls import reverse
 from rest_framework import status
-from datetime import datetime
-import json
-from .views import ArticleTagView, PrivateArticleView, JobArticleView
-from unittest.mock import patch
-from django.test import TestCase, Client
+from django.test import Client
+from .models import ArticleTag, PrivateArticle, JobArticle
 
 
 class BlogApiTest(APITestCase):
@@ -45,16 +41,26 @@ class BlogApiTest(APITestCase):
             "tag_name": "UnitTest Tag",
             "tag_type": 1
         }
+        initial_count = ArticleTag.objects.count()
         response = self.client.post('/tags/', data, content_type='application/json')
         # ステータスコードが 201 Created であることを確認
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        # アサーション: タグがデータベースに存在することを確認
+        self.assertTrue(ArticleTag.objects.filter(tag_name='UnitTest Tag').exists())
+        # アサーション: レコード数が1増加していることを確認
+        self.assertEqual(ArticleTag.objects.count(), initial_count + 1)
 
     def test_article_tag_view_post_invalid(self):
         # 無効なデータで POST リクエストを作成
         data = {'invalid_field': 'some_value'}
+        initial_count = ArticleTag.objects.count()
         response = self.client.post('/tags/', data, content_type='application/json')
         # エラーレスポンスが返ってくることを確認
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        # アサーション: データベースにタグが追加されていないことを確認
+        self.assertFalse(ArticleTag.objects.filter(tag_name='some_value').exists())
+        # アサーション: レコード数が変化していないことを確認
+        self.assertEqual(ArticleTag.objects.count(), initial_count)
 
     # PrivateArticleView のテストケース
     def test_private_article_view_get_all(self):
@@ -80,16 +86,24 @@ class BlogApiTest(APITestCase):
             "title": "Private Article Title",
             "body": "Private Article Body"
         }
+        initial_count = PrivateArticle.objects.count()
         response = self.client.post('/private_articles/', data, content_type='application/json')
         # ステータスコードが 201 Created であることを確認
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        # アサーション: プライベート記事がデータベースに存在することを確認
+        self.assertTrue(PrivateArticle.objects.filter(title='Private Article Title').exists())
+        # アサーション: レコード数が1増加していることを確認
+        self.assertEqual(PrivateArticle.objects.count(), initial_count + 1)
 
     def test_private_article_view_post_invalid(self):
         # 無効なデータで POST リクエストを作成
         data = {'invalid_field': 'some_value'}
+        initial_count = PrivateArticle.objects.count()
         response = self.client.post('/private_articles/', data, content_type='application/json')
         # エラーレスポンスが返ってくることを確認
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        # アサーション: レコード数が変化していないことを確認
+        self.assertEqual(PrivateArticle.objects.count(), initial_count)
 
     # JobArticleView のテストケース
     def test_job_article_view_get_all(self):
@@ -111,17 +125,25 @@ class BlogApiTest(APITestCase):
     def test_job_article_view_post_valid(self):
         # 有効なデータで POST リクエストを作成
         data = {
-            "job_tag_names": "JobTag1, JobTag2",
+            "job_tag_names": "Tag1, Tag2",
             "title": "Job Article Title",
             "body": "Job Article Body"
         }
+        initial_count = JobArticle.objects.count()
         response = self.client.post('/job_articles/', data, content_type='application/json')
         # ステータスコードが 201 Created であることを確認
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        # アサーション: ジョブ記事がデータベースに存在することを確認
+        self.assertTrue(JobArticle.objects.filter(title='Job Article Title').exists())
+        # アサーション: レコード数が1増加していることを確認
+        self.assertEqual(JobArticle.objects.count(), initial_count + 1)
 
     def test_job_article_view_post_invalid(self):
         # 無効なデータで POST リクエストを作成
         data = {'invalid_field': 'some_value'}
+        initial_count = JobArticle.objects.count()
         response = self.client.post('/job_articles/', data, content_type='application/json')
         # エラーレスポンスが返ってくることを確認
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        # アサーション: レコード数が変化していないことを確認
+        self.assertEqual(JobArticle.objects.count(), initial_count)
